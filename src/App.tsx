5e83b843e7c851872f73adf4cc9f42742fdd1739
DenTex0 <denis200358@gmail.com>
2022-12-02 00:46:49 +0200

    removed imgs

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,6 @@ function App() {
 
   return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
