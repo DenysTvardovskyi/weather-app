6b6a7903cf37738350e16e419d720b9022984cb1
DenTex0 <denis200358@gmail.com>
2022-12-07 15:24:11 +0200

    fixed mocked data

--- test/src/views/components/weather-info.spec.tsx
+++ test/src/views/components/weather-info.spec.tsx
@@ -5,39 +5,16 @@ import * as React from 'react';
 describe('weather-info', () => {
   it('renders correctly', () => {
     const mockedData = {
-      dt: 1670360400,
-      main: {
-        temp: 1.13,
-        feels_like: -1.46,
-        temp_min: 1.13,
-        temp_max: 1.13,
-        pressure: 1014,
-        sea_level: 1014,
-        grnd_level: 982,
-        humidity: 98,
-        temp_kf: 0,
+      temp: 1.13,
+      weather: {
+        id: 804,
+        main: 'Clouds',
+        description: 'overcast clouds',
+        icon: '04n',
       },
-      weather: [
-        {
-          id: 804,
-          main: 'Clouds',
-          description: 'overcast clouds',
-          icon: '04n',
-        },
-      ],
-      clouds: {
-        all: 100,
-      },
-      wind: {
-        speed: 2.28,
-        deg: 139,
-        gust: 5.12,
-      },
-      visibility: 1009,
+      humidity: 98,
+      wind_speed: 2.28,
       pop: 0.33,
-      sys: {
-        pod: 'n',
-      },
       dt_txt: '2022-12-06 21:00:00',
     };
     const name = 'Test';
